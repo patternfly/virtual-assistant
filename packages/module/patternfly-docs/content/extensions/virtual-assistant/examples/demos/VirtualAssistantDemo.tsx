35cb5a54a2edbf247da24ac7372cf968519f6a0d
nicolethoen <nthoen@redhat.com>
2024-08-06 15:20:48 -0400

    feat(Brand): migrate brand component

--- packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/demos/VirtualAssistantDemo.tsx
+++ packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/demos/VirtualAssistantDemo.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ChatbotToggle from '@patternfly/virtual-assistant/dist/dynamic/ChatbotToggle';
+import { Brand } from '@patternfly/react-core';
 
 export const BasicDemo: React.FunctionComponent = () => {
   const [ chatbotVisible, setChatbotVisible ] = React.useState<boolean>(false);
   return (
-    <ChatbotToggle toolTipLabel="Chatbot" isChatbotVisible={chatbotVisible} onToggleChatbot={() => setChatbotVisible(!chatbotVisible)} />
+    <>
+      <ChatbotToggle toolTipLabel="Chatbot" isChatbotVisible={chatbotVisible} onToggleChatbot={() => setChatbotVisible(!chatbotVisible)} />
+    </>
   );
 }
