5a9b0f8d25ede7a229e5f50f7391724fd753f09f
nicolethoen <nthoen@redhat.com>
2024-09-10 20:31:42 -0400

    fix: FileDetailsLabel example

--- packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/ChatbotAttachment/FileDetailsLabel.tsx
+++ packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/ChatbotAttachment/FileDetailsLabel.tsx
@@ -5,14 +5,19 @@ import { FormGroup } from '@patternfly/react-core/dist/dynamic/FormGroup';
 import { Stack } from '@patternfly/react-core/dist/dynamic/Stack';
 
 export const FileDetailsLabelExample: React.FunctionComponent = () => {
-  const labelState = React.useState('plain');
+  const [variant, setVariant] = React.useState('plain');
+
+  const onChange = (e, newVariant) => {
+    console.log(newVariant);
+    setVariant(newVariant);
+  }
 
   return (
     <Stack hasGutter>
       <FormGroup role="radiogroup" isInline fieldId="basic-form-radio-group" label="Variant">
-        <Radio name="basic-inline-radio" label="Plain" id="plain" />
-        <Radio name="basic-inline-radio" label="Clickable" id="clickable" />
-        <Radio name="basic-inline-radio" label="Loading" id="loading" />
+        <Radio isChecked={variant === 'plain'} onChange={() => setVariant('plain')} name="basic-inline-radio" label="Plain" id="plain" />
+        <Radio isChecked={variant === 'clickable'} onChange={() => setVariant('clickable')} name="basic-inline-radio" label="Clickable" id="clickable" />
+        <Radio isChecked={variant === 'loading'} onChange={() => setVariant('loading')} name="basic-inline-radio" label="Loading" id="loading" />
       </FormGroup>
       <div className="pf-chatbot__file-details-example">
         <FileDetailsLabel
@@ -20,8 +25,8 @@ export const FileDetailsLabelExample: React.FunctionComponent = () => {
           // eslint-disable-next-line no-console
           onClose={() => console.log('clicked close button!')}
           // eslint-disable-next-line no-console
-          {...(labelState === 'clickable' && { onClick: () => console.log('clicked entire button!') })}
-          {...(labelState === 'loading' && { isLoading: true })}
+          {...(variant === 'clickable' && { onClick: () => console.log('clicked entire button!') })}
+          {...(variant === 'loading' && { isLoading: true })}
         />
       </div>
     </Stack>
