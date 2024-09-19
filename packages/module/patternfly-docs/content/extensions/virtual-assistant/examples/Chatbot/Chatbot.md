1d6da97d196f34f7e31fa80b48f509960e1471ba
nicolethoen <nthoen@redhat.com>
2024-09-19 10:21:17 -0400

    docs: chatbot docs

--- packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/Chatbot/Chatbot.md
+++ packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/Chatbot/Chatbot.md
@@ -1,3 +1,4 @@
+---
 # Sidenav top-level section
 # should be the same for all markdown files
 section: extensions
@@ -10,13 +11,44 @@ source: react
 # If you use typescript, the name of the interface to display props for
 # These are found through the sourceProps function provided in patternfly-docs.source.js
 propComponents: [
-'Chatbot'
+'Chatbot',
+'ChatbotContent', 
+'MessageBox',
+'ChatbotWelcomePrompt',
+'WelcomePrompt'
 ]
 ---
+
 import Chatbot, { ChatbotDisplayMode } from '@patternfly/virtual-assistant/dist/dynamic/Chatbot';
+import ChatbotWelcomePrompt from '@patternfly/virtual-assistant/dist/dynamic/ChatbotWelcomePrompt';
 
 ### Chatbot container in various display modes
 
 ```js file="./ChatbotContainer.tsx" isFullscreen
+
+```
+
+### Chatbot content and message box
+
+The `ChatbotContent` component is the container that is placed within the `Chatbot` between the [ChatbotHeader](/extensions/chat-bots--ai/chatbot-header) and [ChatbotFooter](/extensions/chat-bots--ai/chatbot-footer).
+It usually contains a `ChatbotMessageBox` for displaying messages.
+
+```noLive
+<Chatbot>
+  <ChatbotHeader ... />
+  <ChatbotContent>
+    <ChatbotMessageBox>
+    ...
+    <ChatbotMessageBox>
+  </ChatbotContent>
+  <ChatbotFooter ... />
+</Chatbot> 
+```
+
+### Chatbot welcome prompt
+
+The welcome prompt fills the message box before the user has input their first message to the chatbot. It can contain pre determined prompts
+
+```js file="./ChatbotWelcomePrompt.tsx"
 
 ```
--- packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/Chatbot/ChatbotContainer.tsx
+++ packages/module/patternfly-docs/content/extensions/virtual-assistant/examples/Chatbot/ChatbotContainer.tsx
@@ -6,25 +6,33 @@ import { FormGroup } from '@patternfly/react-core/dist/dynamic/FormGroup';
 import { Stack } from '@patternfly/react-core/dist/dynamic/Stack';
 
 export const ChatbotContainerDemo: React.FunctionComponent = () => {
-  const [displayMode, setDisplayMode] = React.useState(ChatbotDisplayMode.embedded);
+  const [displayMode, setDisplayMode] = React.useState(ChatbotDisplayMode.default);
   const [isVisible, setIsVisible] = React.useState(true);
 
   return (
-    <div>
-      <Stack hasGutter>
-        <FormGroup role="radiogroup" isInline fieldId="displayModeRadios" label="Display mode">
-          <Radio isChecked={variant === ChatbotDisplayMode.embedded} onChange={() => setVariant(ChatbotDisplayMode.embedded)} name="basic-inline-radio" label="Embedded" id="embedded" />
-          <Radio isChecked={variant === ChatbotDisplayMode.docked} onChange={() => setVariant(ChatbotDisplayMode.docked)} name="basic-inline-radio" label="Docked" id="docked" />
-          <Radio isChecked={variant === ChatbotDisplayMode.fullscreen} onChange={() => setVariant(ChatbotDisplayMode.fullscreen)} name="basic-inline-radio" label="Fullscreen" id="fullscreen" />
-        </FormGroup>
-      </Stack>
-      <Checkbox
-        label="Display chatbot container"
-        isChecked={isVisible}
-        onChange={() => setIsVisible(!isVisible)}
-        id="container-visible"
-        name="container-visible"
-      />
-    </div>
+    <>
+      <div style={{
+        position: 'fixed',
+        padding: 'var(--pf-t--global--spacer--lg)',
+        zIndex: '601',
+        boxShadow: 'var(--pf-t--global--box-shadow--lg)'
+      }}>
+        <Stack hasGutter>
+          <FormGroup role="radiogroup" isInline fieldId="displayModeRadios" label="Display mode">
+            <Radio isChecked={displayMode === ChatbotDisplayMode.default} onChange={() => setDisplayMode(ChatbotDisplayMode.default)} name="basic-inline-radio" label="Overlay / default" id="default" />
+            <Radio isChecked={displayMode === ChatbotDisplayMode.docked} onChange={() => setDisplayMode(ChatbotDisplayMode.docked)} name="basic-inline-radio" label="Docked" id="docked" />
+            <Radio isChecked={displayMode === ChatbotDisplayMode.fullscreen} onChange={() => setDisplayMode(ChatbotDisplayMode.fullscreen)} name="basic-inline-radio" label="Fullscreen" id="fullscreen" />
+          </FormGroup>
+          <Checkbox
+            label="Display chatbot container"
+            isChecked={isVisible}
+            onChange={() => setIsVisible(!isVisible)}
+            id="container-visible"
+            name="container-visible"
+          />
+        </Stack>
+      </div>
+      <Chatbot displayMode={displayMode} isVisible={isVisible}></Chatbot>
+    </>
   )
 }
